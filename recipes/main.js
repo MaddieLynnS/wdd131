e411503533c1325f3c29e3c5d42fd139902a367c
Maddie :) <maddiemunch123@gmail.com>
2025-03-15 20:53:02 -0600

    RecipeBook can filter and search!

---
+++ recipes/main.js
@@ -0,0 +1,87 @@
+import recipes from "./recipes.mjs";
+
+function getRandomNumber() {
+    //may need plus one, we shall see
+    return Math.floor(Math.random() * recipes.length);
+}
+
+function getRandomRecipe(listOfRecipes) {
+    return listOfRecipes[getRandomNumber()];
+}
+
+function getRecipeTemplate(recipe) {
+    return `<section class="recipe-container">
+    <article class="recipe">
+                    <div>
+                        <img src="${recipe.image}" alt="${recipe.name}">
+                    </div>
+                    <div class="recipe-info">
+                        <div class="tags">
+                            ${tagTemplate(recipe.tags)}
+                        </div>
+                        <h3><a href="#">${recipe.name}</a></h3>
+                        <span
+                            class="rating"
+                            role="img"
+                            aria-label="Rating: ${recipe.rating} out of 5 stars"
+                            >
+                            ${ratingTemplate(recipe.rating)}
+                        </span>
+                        <p>${recipe.description}</p>
+                    </div>
+                </article>
+                </section>`;
+}
+
+function tagTemplate(tags) {
+    let html = ``;
+    tags.forEach(tag => {
+        html += `<p id="tag">${tag}</p>\n`;
+    });
+    return html;
+}
+
+function ratingTemplate(rating) {
+	let html = ``;
+    for(let i = 1; i < 6; i++) {
+        html += `<span aria-hidden="true" class="icon-star`;
+        html += (i <= rating ? `">⭐` : `-empty">☆`);
+        html += `</span>\n`;
+    }
+    return html;
+}
+
+function renderRecipes(recipeList) {
+
+    const recipeElement = document.querySelector(".recipes");
+    const html = recipeList.map((recipe) => getRecipeTemplate(recipe));
+    recipeElement.innerHTML = html.join("");
+
+}
+
+function init() {
+  const recipe = getRandomRecipe(recipes)
+  renderRecipes([recipe]);
+}
+init();
+
+function filterRecipes(query) {
+    function searchCallback(recipe)
+    {
+        return (recipe.name.toLowerCase().includes(query)
+      || recipe.description.toLowerCase().includes(query)
+      || recipe.tags.find((tag) => tag.toLowerCase().includes(query))
+      || recipe.recipeIngredient.find((ingredient) => ingredient.toLowerCase().includes(query)));
+    }
+    const filteredRecipes = recipes.filter(searchCallback);
+    filteredRecipes.sort((a, b) => a.name.localeCompare(b.name));
+
+    renderRecipes(filteredRecipes);
+}
+
+function searchHandler() {
+    let userInput = document.getElementById("recipe-search").value;
+    filterRecipes(userInput.toLowerCase());
+}
+
+document.querySelector("#search-image").addEventListener("click", searchHandler);
